6f76cb5a93408add5edfbefd1c949dc12be2df01
dotcamdev <dotcamdev@users.noreply.github.com>
2018-06-01 12:49:16 -0700

    Added a blank UE4 project for dev work

---
+++ Source/VaOcean/VaOcean.Build.cs
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class VaOcean : ModuleRules
+{
+	public VaOcean(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicIncludePaths.AddRange(
+			new string[] { 
+				// ... add public include paths required here ...
+			}
+		);
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"VaOceanPlugin/Private"
+                 // ... add other private include paths required here ...
+			}
+		);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+                "Engine",
+                "RenderCore",
+                "ShaderCore",
+                "RHI"
+                // ... add other public dependencies that you statically link with here ...
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...
+			}
+		);
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+		);
+	}
+}
